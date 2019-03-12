2e74ca5d4f421d4fa613385541c60955dbae564b
okjacky <ookjacky@gmail.com>
2019-03-12 16:33:05 +0100

    config mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :attendances
   has_many :gigs, through: :attendances
+
+  
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
