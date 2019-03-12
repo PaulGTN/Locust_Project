ea51397f38c5d446872e1c4816cdcf81032138d5
Paul.Graton <paulgtn@hotmail.fr>
2019-03-12 12:41:33 +0100

    Seed Done

--- db/migrate/20190311150350_create_gigs.rb
+++ db/migrate/20190311150350_create_gigs.rb
@@ -3,6 +3,7 @@ class CreateGigs < ActiveRecord::Migration[5.2]
     create_table :gigs do |t|
       t.string :name
       t.datetime :date
+      t.belongs_to :bar, index: true
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+Bar.destroy_all
+Gig.destroy_all
+Tag.destroy_all
+Price.destroy_all
+GigTag.destroy_all
+BarTag.destroy_all
+Attendance.destroy_all
+
+puts 'Start seeding...'
+
+1.times do 
+  price = Price.create!(price_range: "moyen")
+end
+puts "1 range created"
+
+15.times do 
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+puts "15 users created"
+
+10.times do 
+  tag = Tag.create!(name: Faker::Music.genre)
+end 
+puts "10 tags created"
+
+10.times do 
+  bar = Bar.create!(name: Faker::Music.album, adress: Faker::Address.street_address, zip_code: Faker::Address.zip_code, city: Faker::Address.city ,price_id: Price.all.ids.sample)
+end 
+puts "10 bars created"
+
+20.times do 
+  gig = Gig.create!(name: Faker::Music.band, date: Faker::Date.forward(10), bar_id: Bar.all.ids.sample)
+end
+puts "20 gigs created"
+
+5.times do 
+  gig_tag = GigTag.create!(tag_id: Tag.all.ids.sample, gig_id: Gig.all.ids.sample)
+end
+puts "5 gig-tags created"
+
+5.times do 
+  bar_tag = BarTag.create!(tag_id: Tag.all.ids.sample, bar_id: Bar.all.ids.sample)
+end
+puts "5 bar-tags created"
+
+20.times do
+  attendance = Attendance.create!(user_id: User.all.ids.sample, gig_id: Gig.all.ids.sample)
+end
+puts "20 attendances created"
