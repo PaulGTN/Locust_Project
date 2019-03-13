3b1e1e58d8407d083859c1e6816e1e4bcf945393
Paul.Graton <paulgtn@hotmail.fr>
2019-03-13 19:46:32 +0100

    Edit Show Bar

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -4,16 +4,16 @@ class BarsController < ApplicationController
     @tags = Tag.all
     @bars = Bar.last(10)
     @prices = Price.all
-    @i = 1
-    @tags_orders = @tags.sort
-    @styles = Bar.find_by(params[:id])
-    @bar_tags = @styles.bar_tags
+    #@i = 1
+    #@tags_orders = @tags.sort
+    #@styles = Bar.find_by(params[:id])
+    #@bar_tags = @styles.bar_tags
   end
 
   def show
     @bar = Bar.find(params[:id])
-    #@tags = @bar.tags
-    #@gigs = @bar.gigs
+    @gigs = @bar.gigs
+
   end
 
 end
