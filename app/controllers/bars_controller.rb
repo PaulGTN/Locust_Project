4e0ee9c4e018c8352836915870bbe8f30394e84e
Steph Ravo <hsravo1@gmail.com>
2019-03-14 13:05:43 +0100

    not working

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -43,7 +43,7 @@ class BarsController < ApplicationController
 	  def authenticate_user
 	    unless current_user
 	      flash[:danger] = "Connectez-vous pour avoir accès à cette fonctionnalité !"
-	      redirect_to new_session_path
+	      redirect_to root_path
 	    end
 	  end
 
