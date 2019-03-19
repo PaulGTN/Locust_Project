b6d378b1403db8d4f0c882fe33c52b111cc056f5
Steph Ravo <hsravo1@gmail.com>
2019-03-19 15:38:51 +0100

    editing profile bars gigs img

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+		@bar = Bar.find(params[:id])
 	end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,36 @@
 Rails.application.routes.draw do
+
   devise_for :users
 
   root 'bars#index'
 
   resources :users do
-    resources :avatars, only: [:create]
-    resources :gigs do
-      resources :attendances
-    end
-  end
-
-  resources :users do
-    resources :bars do
-      resources :favorites
-    end
-  end
-
-  resources :bars do
-    resources :barpicture1
-    resources :barpicture2
-  end   
-
-  resources :gigs do
-    resources :gigpicture
+    resources :avatars, only: [:create] 
   end
 
   resources :attendances, only: %i[create new destroy]
   resources :favorites, only: %i[new create destroy]
+  resources :bars, only: %i[show index] do
+      resources :barpicture1 
+      resources :barpicture2
+  end
+
+  resources :gigs, only: %i[show index] do
+    resources :gigpicture
+    end
 
   scope 'admin', module: 'admin', as: 'admin' do
+
     root 'bars#index'
-    resources :bars
-    resources :gigs
+
+    resources :bars  do
+      resources :barpicture1 
+      resources :barpicture2
+    end
+
+    resources :gigs do
+    resources :gigpicture
+    end
   end
+
 end
