7a32d96b73b493e0a58f83b89e84e1e51024405f
Steph Ravo <hsravo1@gmail.com>
2019-03-19 18:51:50 +0100

    back to local

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_122222) do
+ActiveRecord::Schema.define(version: 2019_03_16_142448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,8 +117,6 @@ ActiveRecord::Schema.define(version: 2019_03_19_122222) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "role"
-    t.string "provider"
-    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
