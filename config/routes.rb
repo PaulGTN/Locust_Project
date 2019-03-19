20aaf46b32b8be07fee6b9057646fc094da44415
Steph Ravo <hsravo1@gmail.com>
2019-03-19 11:48:49 +0100

    routes down

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,36 @@
 Rails.application.routes.draw do
-
   devise_for :users
 
   root 'bars#index'
 
   resources :users do
-    resources :avatars, only: [:create] do
-      resources :gigs do
-          resources :attendances
-    end
+    resources :avatars, only: [:create]
+    resources :gigs do
+      resources :attendances
     end
   end
 
   resources :users do
-      resources :bars do
-          resources :favorites
+    resources :bars do
+      resources :favorites
+    end
   end
+
+  resources :bars do
+    resources :barpicture1
+    resources :barpicture2
+  end   
+
+  resources :gigs do
+    resources :gigpicture
   end
 
   resources :attendances, only: %i[create new destroy]
   resources :favorites, only: %i[new create destroy]
-  resources :bars, only: %i[show index]
-  resources :gigs, only: %i[show index]
 
   scope 'admin', module: 'admin', as: 'admin' do
-
     root 'bars#index'
-
-    resources :bars  do
-      resources :barpicture1
-      resources :barpicture2
-  end
-
-    resources :gigs do
-    resources :gigpicture
-  end
+    resources :bars
+    resources :gigs
   end
 end
