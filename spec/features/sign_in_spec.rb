1cc5f22d1caa6103b1bc6aba573653a8a33496b7
Paul.Graton <paulgtn@hotmail.fr>
2019-03-22 12:45:21 +0100

    Last Test

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,28 +1,15 @@
-require "rails_helper"
+require 'rspec/rails'
+require 'capybara/rails'
 
-RSpec.feature "Widget management", :type => :feature do
-=begin
-describe "the sign_in process", type: :feature do
-  before :each do
-    User.create(email: 'user@example.com', password: 'password')
+describe "the signin process", type: :feature do
+  it 'sign me in' do
+
+    visit 'users/sign_in'
+    fill_in "user[email]", with: "testing@email.com"
+    fill_in "user[password]", with: "motdepasse"
+    click_button "Connexion"
+
+    expect(page).to have_content('Connexion')
+
   end
-
-  it "signs me in" do
-    # on va sur la page de création d'utilisateurs
-    visit '/users/sign_in'
-
-    # dans le formulaire des users, on remplit les données qu'il faut
-    within("#user") do
-      fill_in 'Votre adresse mail', with: 'user@example.com'
-      fill_in 'Votre mot de passe', with: 'password'
-    end
-
-    # clik clik
-    click_button 'Connexion'
-
-    # la page affichée devrait afficher des bonnes nouvelles
-    expect(page).to have_content 'Success'
-  end
-end
-=end
 end
